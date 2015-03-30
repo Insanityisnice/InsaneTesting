efea127f0699dd92ccd444203905f3a2e11af5f9
James Stackhouse <insanityisnice@hotmail.com>
2015-03-29 21:23:40 -0700

    Add OWIN test host for testing OWIN hosted WebApi.

---
+++ source/Insanity.Testing.Integration.Owin/OwinApiHost.cs
@@ -0,0 +1,41 @@
+﻿using Insanity.Testing.Integration.Http;
+using Microsoft.Owin.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insanity.Testing.Integration.Owin
+{
+	public class OwinApiHost
+	{
+		private string baseAddress;
+		private TestServer host;
+		private string jwtToken;
+
+		public ApiClient Client
+		{
+			get
+			{
+				return new ApiClient(baseAddress, host.HttpClient, jwtToken);
+			}
+		}
+
+		public OwinApiHost(string baseAddress, string jwtToken = null)
+		{
+			this.baseAddress = baseAddress;
+			this.jwtToken = jwtToken;
+		}
+
+		public void Start<TStartup>()
+		{
+			host = Microsoft.Owin.Testing.TestServer.Create<TStartup>();
+		}
+
+		public void Stop()
+		{
+			host.Dispose();
+		}
+	}
+}
