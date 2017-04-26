ae2df2f69809403a6b13a1d3fe99c2f5ae3b93e7
James Stackhouse <insanityisnice@hotmail.com>
2017-04-25 21:04:30 -0700

    batch file for publishing nuget package.

---
+++ source/Insanity.Testing.Integration.Data/publish_nuget.bat
@@ -0,0 +1,2 @@
+﻿nuget pack .\Insanity.Testing.Integration.Data.csproj -properties Configuration=Release
+nuget push .\Insanity.Testing.Integration.Data.1.0.0.nupkg -Source https://www.nuget.org/api/v2/package
